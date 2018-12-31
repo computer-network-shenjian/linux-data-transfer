886dee98fe6696c14ab904cd723cdddd0ace8a2f
Novate Z <22500949+novate@users.noreply.github.com>
2018-12-31 12:02:28 +0800

    test read dat ok

--- test/test_dat/test.cpp
+++ test/test_dat/test.cpp
@@ -4,11 +4,27 @@ using namespace std;
 int main() {
     unsigned int buflen = 20;
     unsigned char *buf = new unsigned char[20];
-    buf[0] = 0x12;
-    buf[1] = 0x3F;
-    buf[2] = 0xEA;
+    buf[0] = 'h';
+    buf[1] = 'e';
+    buf[2] = 'l';
+    buf[3] = 'l';
+    buf[4] = 'o';
     write_dat(buf, buflen, kRawDat);
     delete[] buf;
+
+    buf = new unsigned char[20];
+    cout << "dat length: " << read_dat(buf, kRawDat) << endl;
+    cout << buf << endl;
+    delete[] buf;
+
+    buf = new unsigned char[1024];
+    buflen = read_dat(buf, kReadDat);
+    cout << "dat length: " << buflen << endl;
+
+    write_dat(buf, buflen, kRawDat);
+
+    delete[] buf;
+
     return 0;
 }
 
@@ -27,4 +43,36 @@ bool write_dat(const unsigned char *buf, const unsigned int buflen, const char *
     }
     ofs.close();
     return true;
+}
+
+int read_dat(unsigned char *buf, const char *dat_name) {
+    ifstream ifs(dat_name);
+    if (!ifs.is_open()) {
+        cerr << "read_dat(): open file error" << endl;
+        return -1;
+    }
+    
+    // b[0] is the first char of hex, b[1] is the second char of hex.
+    char b[3] = {0};
+    int cnt = 0;
+    while (ifs.good()) {
+        b[0] = ifs.get();
+        if (b[0] == -1) {
+            break;
+        }
+        if (b[0] == '\n') {
+            b[0] = ifs.get();
+        }
+        b[1] = ifs.get();
+        ifs.get();
+        stringstream ss;
+        ss << b;
+        unsigned int a;
+        ss >> hex >> a;
+        buf[cnt] = a;
+        cnt++;
+    }
+
+    ifs.close();
+    return cnt;
 }
--- test/test_dat/test.hpp
+++ test/test_dat/test.hpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <sstream>
 #include <iomanip>
 #include <fstream>
 #include <stdint.h>
 
 #define kRawDat "sender.dat"
+#define kReadDat "../../network1.dat"
 
 bool write_dat(const unsigned char *buf, const unsigned int buflen, const char *dat_name);
+
+int read_dat(unsigned char *buf, const char *dat_name);
